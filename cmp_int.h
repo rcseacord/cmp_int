b04a03311ff4f6328f4cfb9466fabf54e6e6ca4b
Robert C. Seacord <rcseacord@gmail.com>
2021-11-09 09:55:35 -0500

    Update cmp_int.h
    reimplemented cmp_less and cmp_equal to not depend on gnu extensions and corrected parenthesis.

--- cmp_int.h
+++ cmp_int.h
@@ -60,31 +60,21 @@
   _Static_assert(is_integer(lhs), "lhs must be an integer"); \
   _Static_assert(is_integer(rhs), "rhs must be an integer");
 
-#define cmp_equal(lhs, rhs) ({                      \
-  check_types(lhs, rhs);                             \
-  int res = 0;                                       \
-  if (is_signed(lhs) == is_signed(rhs))              \
-    res = lhs == rhs;                                \
-  else if (is_signed(lhs))                           \
-    res = lhs < 0 ? 0 : make_unsigned(lhs) == rhs;   \
-  else                                               \
-    res = rhs < 0 ? 0 : lhs == make_unsigned(rhs);   \
-  res;                                               \
-})
+#define cmp_equal(lhs, rhs) \
+  ((void)sizeof(struct { check_types(lhs, rhs); int dummy; }), \
+   (is_signed(lhs) == is_signed(rhs) ? (lhs) == (rhs) : \
+    is_signed(lhs) ? \
+      (lhs) < 0 || make_unsigned(lhs) == (rhs) : \
+      ((rhs) >= 0 && (lhs) == make_unsigned(rhs))))
 
 #define cmp_not_equal(lhs, rhs) !cmp_equal(lhs, rhs)
  
-#define cmp_less(lhs, rhs) ({                        \
-  check_types(lhs, rhs);                             \
-  int res = 0;                                       \
-  if (is_signed(lhs) == is_signed(rhs))              \
-    res = lhs < rhs;                                 \
-  else if (is_signed(lhs))                           \
-    res = lhs < 0 ? 1 : make_unsigned(lhs) < rhs;    \
-  else                                               \
-    res = rhs < 0 ? 0 : lhs < make_unsigned(rhs);    \
-  res;                                               \
-})
+#define cmp_less(lhs, rhs) \
+  ((void)sizeof(struct { check_types((lhs), (rhs)); int dummy; }), \
+  (is_signed(lhs) == is_signed(rhs) ? (lhs) < (rhs) : \
+    is_signed(lhs) ? \
+      (lhs) < 0 || make_unsigned(lhs) < (rhs) : \
+      ((rhs) >= 0 && (lhs) < make_unsigned(rhs))))
 
 #define cmp_greater(lhs, rhs) cmp_less(rhs, lhs)
 #define cmp_less_equal(lhs, rhs) !cmp_greater(lhs, rhs)
