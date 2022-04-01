2ec06398cc7b98a6aa16839359f194994e510a49
Anton Gerasimov <ant.gerasimov1@gmail.com>
2022-04-01 11:43:32 -0400

    updated ctest

--- test/CTestTestfile.cmake
+++ test/CTestTestfile.cmake
@@ -4,5 +4,7 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(test_cmp_equal "TestCmpInt")
-set_tests_properties(test_cmp_equal PROPERTIES  _BACKTRACE_TRIPLES "/home/kali/cmp_int/test/CMakeLists.txt;7;add_test;/home/kali/cmp_int/test/CMakeLists.txt;0;")
+add_test(Arithmetical_Tests "TestCmpInt")
+set_tests_properties(Arithmetical_Tests PROPERTIES  _BACKTRACE_TRIPLES "/home/kali/cmp_int/test/CMakeLists.txt;7;add_test;/home/kali/cmp_int/test/CMakeLists.txt;0;")
+add_test(Standard_Library_Tests "TestStdLib")
+set_tests_properties(Standard_Library_Tests PROPERTIES  _BACKTRACE_TRIPLES "/home/kali/cmp_int/test/CMakeLists.txt;8;add_test;/home/kali/cmp_int/test/CMakeLists.txt;0;")
