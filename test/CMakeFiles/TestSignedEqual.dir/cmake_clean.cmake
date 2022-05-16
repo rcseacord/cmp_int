1b99b6e1a14859c9d5f36ef6def87cfcc10d08fb
Anton Gerasimov <ant.gerasimov1@gmail.com>
2022-05-16 07:37:04 -0400

    signed tests

---
+++ test/CMakeFiles/TestSignedEqual.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestSignedEqual.dir/signed_cmp_equal.c.o"
+  "CMakeFiles/TestSignedEqual.dir/signed_cmp_equal.c.o.d"
+  "TestSignedEqual"
+  "TestSignedEqual.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TestSignedEqual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
